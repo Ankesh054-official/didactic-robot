93e927b436125fb3ad1bf9e304a02fc6c5f4c74b
ANKESH <ankeshsharma@pm.me>
2024-07-25 12:04:40 +0530

    Added README.md

--- src/CodeNotes/string/Main.java
+++ src/CodeNotes/string/Main.java
@@ -1,5 +1,11 @@
 package CodeNotes.string;
 
 public class Main {
+    public static void main(String[] args) {
+        String str = new String("Ankesh");
+        String st = "Ankesh";
+        System.out.println(st);
 
+        System.out.println(str);
+    }
 }
