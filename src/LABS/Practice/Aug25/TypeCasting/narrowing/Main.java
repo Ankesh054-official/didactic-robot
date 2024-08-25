0e7485653c927c9abe90f164fa88b5d26de731b2
ANKESH <ankeshsharma@pm.me>
2024-08-25 11:32:51 +0530

    Type casting: Which is used to convert one data type to another, converting lower to higher known as widning/Implicit casting and converting higher to lower known as narrowing/Explicit casting. (Here lower to higher means data storage capacity of a data type) Note: In Explicit casting cast opertor is used (datatype) in which we are trying to convert.

--- src/LABS/Practice/Aug25/TypeCasting/narrowing/Main.java
+++ src/LABS/Practice/Aug25/TypeCasting/narrowing/Main.java
@@ -1,2 +1,19 @@
-package LABS.Practice.Aug25.TypeCasting.narrowing;public class Main {
+package LABS.Practice.Aug25.TypeCasting.narrowing;
+
+
+/**
+ * Narrowing
+ * <p>{@code double -> float -> long -> int -> short -> byte}</p>
+ * <p>I'll  return value of char according to ascii value.</p>
+ * <p>{@code int -> char}</p>
+ *
+ * {@code (float)} This is called cast operator this is used to perform narrowing.
+ */
+public class Main {
+    public static void main(String[] args) {
+//        Float.MAX_VALUE == 3.4028235E38
+                double a = 3.4028235E38;
+                float b = (float) a;// Exceeding by only one after decimal result in Infinity.
+        System.out.println(b);
+    }
 }
--- src/LABS/Practice/Aug25/TypeCasting/widning/Main.java
+++ src/LABS/Practice/Aug25/TypeCasting/widning/Main.java
@@ -1,2 +1,24 @@
-package LABS.Practice.Aug25.TypeCasting.widning;public class Main {
+package LABS.Practice.Aug25.TypeCasting.widning;
+
+public class Main {
+
+    /**
+     * <p>{@code byte -> short -> int -> long -> float -> double}</p>
+     *
+     * It'll print value of char data according to ascii values.
+     * <p>{@code char -> int}</p>
+     */
+    public static void main(String[] args) {
+        byte a = 10;
+        short b = a;
+        int c = b;
+        long d = c;
+        float e = d;
+        double f = e;
+
+        char j = 'a';
+        int h = j;
+        System.out.println(h);
+
+    }
 }
